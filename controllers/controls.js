6f6c5a39a002ff11eec897cc5d7e624755d147b5
marcee <marceloquadrilatero@gmail.com>
2025-11-22 23:22:35 -0300

    fix funcion fecha hora simple

--- controllers/controls.js
+++ controllers/controls.js
@@ -1,43 +1,55 @@
-import { pool } from "../db/db.js"
-
+import { pool } from "../db/db.js";
 
 export const testing = (req, res) => {
-    res.send("El servidor funciona correctamente")
-}
+  res.send("El servidor funciona correctamente");
+};
 
-export const prueba = async(req, res) => {
-    try{
+export const prueba = async (req, res) => {
+  try {
+    await pool.query(
+      "INSERT INTO lavados (marca, vin, ubicacion, tiempo) VALUES (?,?,?,?)",
+      [
+        req.body.marca,
+        req.body.vin,
+        req.body.ubicacion,
+        req.body.tiempoFOrmateado,
+      ]
+    );
+    res.send("Ruta de prueba funcionando");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-        await pool.query ("INSERT INTO lavados (marca, vin, ubicacion, tiempo) VALUES (?,?,?,?)",
-            [req.body.marca, req.body.vin, req.body.ubicacion, req.body.tiempoFOrmateado])
-            res.send("Ruta de prueba funcionando")
-        }catch(error) {
-            console.log(error)
-        }
-    }
+export const getLavados = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM lavados");
 
+    const fechaHoras = rows.map((lav) => {
+      const fecha = new Date(lav.fecha);
 
-export const getLavados = async(req, res) => {
-    try {
-       const [rows] = await pool.query("SELECT * FROM lavados")
-       const fechaFormateadas = rows.map(lav => {
-        const fecha = new Date(lav.fecha);
+      const fechaFormateada = fecha.toLocaleDateString("es-AR", {
+        timeZone: "America/Argentina/Buenos_Aires",
+      });
+
+      const horaFormateda = fecha
+        .toLocaleTimeString("en-US", {
+          timeZone: "America/Argentina/Buenos_Aires",
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        })
+        
       return {
         ...lav,
-        fecha: new Date(lav.fecha).toLocaleString("es-AR", {
-            timeZone: "America/Argentina/Buenos_Aires",
-          day: "2-digit",
-          month: "2-digit",
-          year: "numeric",
-          hour: "2-digit",
-          minute: "2-digit"
-        })
+        fecha: fechaFormateada,
+        hora: horaFormateda,
       };
     });
+    console.log(fechaHoras, "nuea funcion fechahora");
 
-       console.log(fechaFormateadas)
-       res.json(fechaFormateadas)
-    } catch (error) {
-        console.log(error)
-    }
-}
+    res.json(fechaHoras);
+  } catch (error) {
+    console.log(error);
+  }
+};
